79c34ca0b4b559be7918c8ea194376a06c461df4
Samvid Roy <120253840+samvid123@users.noreply.github.com>
2024-06-18 23:50:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+Shery.mouseFollower();
+Shery.makeMagnet(".main-button" /* Element to target.*/, {
+    //Parameters are optional.
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
+  });
